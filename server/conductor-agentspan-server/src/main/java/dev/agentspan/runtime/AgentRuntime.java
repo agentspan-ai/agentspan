d7ebc49ad5c7e376ef6eaa2a5a2d9b9f53651536
Viren Baraiya <viren@v1r3n.com>
2026-07-12 22:58:44 -0700

    Update AgentRuntime.java

--- server/conductor-agentspan-server/src/main/java/dev/agentspan/runtime/AgentRuntime.java
+++ server/conductor-agentspan-server/src/main/java/dev/agentspan/runtime/AgentRuntime.java
@@ -18,19 +18,15 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.netflix.conductor.core.execution.tasks.Join;
 
 import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication(
         exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableScheduling
-@ComponentScan(
-        basePackages = {"com.netflix.conductor", "io.orkes.conductor", "org.conductoross.conductor"})
+@ComponentScan(basePackages = {"com.netflix.conductor", "io.orkes.conductor", "org.conductoross.conductor"})
 @RequiredArgsConstructor
 public class AgentRuntime implements ApplicationRunner {
 
